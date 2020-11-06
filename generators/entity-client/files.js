e4b023fe36b431b5c53424ba564211036fbd34d1
Rob Walls <rob.w3@pm.me>
2020-11-06 13:09:00 -0700

    Added new templates to file array

--- generators/entity-client/files.js
+++ generators/entity-client/files.js
@@ -23,6 +23,151 @@ function getMainFiles(ANGULAR_DIR) {
                     {
                         file: 'entities/entity-management.component.ts',
                         renameTo: generator => `entities/${generator.entityFolderName}/${generator.entityFileName}.component.ts`
+                    },
+                    {
+                        file: 'entities/entity-shell/entity-shell.module.ts',
+                        renameTo: generator =>
+                            `entities/${generator.entityFolderName}/${generator.entityFileName}-shell/${
+                                generator.entityFileName
+                            }-shell.module.ts`
+                    },
+                    {
+                        file: 'entities/entity-shell/entity-shell.component.ts',
+                        renameTo: generator =>
+                            `entities/${generator.entityFolderName}/${generator.entityFileName}-shell/${
+                                generator.entityFileName
+                            }-shell.component.ts`
+                    },
+                    {
+                        file: 'entities/entity-shell/entity-shell.component.html',
+                        method: 'processHtml',
+                        template: true,
+                        renameTo: generator =>
+                            `entities/${generator.entityFolderName}/${generator.entityFileName}-shell/${
+                                generator.entityFileName
+                            }-shell.component.html`
+                    },
+                    {
+                        file: 'entities/entity-shell/entity-shell.component.scss',
+                        renameTo: generator =>
+                            `entities/${generator.entityFolderName}/${generator.entityFileName}-shell/${
+                                generator.entityFileName
+                            }-shell.component.scss`
+                    },
+                    {
+                        file: 'entities/entity-shell/entity-book/entity-book.component.ts',
+                        renameTo: generator =>
+                            `entities/${generator.entityFolderName}/${generator.entityFileName}-shell/${generator.entityFileName}-book/${
+                                generator.entityFileName
+                            }-book.component.ts`
+                    },
+                    {
+                        file: 'entities/entity-shell/entity-book/entity-book.component.scss',
+                        renameTo: generator =>
+                            `entities/${generator.entityFolderName}/${generator.entityFileName}-shell/${generator.entityFileName}-book/${
+                                generator.entityFileName
+                            }-book.component.scss`
+                    },
+                    {
+                        file: 'entities/entity-shell/entity-book/entity-book.component.html',
+                        method: 'processHtml',
+                        template: true,
+                        renameTo: generator =>
+                            `entities/${generator.entityFolderName}/${generator.entityFileName}-shell/${generator.entityFileName}-book/${
+                                generator.entityFileName
+                            }-book.component.html`
+                    },
+                    {
+                        file: 'entities/entity-shell/entity-book/entity-book-page/entity-book-page.component.ts',
+                        renameTo: generator =>
+                            `entities/${generator.entityFolderName}/${generator.entityFileName}-shell/${generator.entityFileName}-book/${
+                                generator.entityFileName
+                            }-book-page/${generator.entityFileName}-book-page.component.ts`
+                    },
+                    {
+                        file: 'entities/entity-shell/entity-book/entity-book-page/entity-book-page.component.scss',
+                        renameTo: generator =>
+                            `entities/${generator.entityFolderName}/${generator.entityFileName}-shell/${generator.entityFileName}-book/${
+                                generator.entityFileName
+                            }-book-page/${generator.entityFileName}-book-page.component.scss`
+                    },
+                    {
+                        file: 'entities/entity-shell/entity-book/entity-book-page/entity-book-page.component.html',
+                        method: 'processHtml',
+                        template: true,
+                        renameTo: generator =>
+                            `entities/${generator.entityFolderName}/${generator.entityFileName}-shell/${generator.entityFileName}-book/${
+                                generator.entityFileName
+                            }-book-page/${generator.entityFileName}-book-page.component.html`
+                    },
+                    {
+                        file: 'entities/entity-shell/entity-grid/entity-grid.component.ts',
+                        renameTo: generator =>
+                            `entities/${generator.entityFolderName}/${generator.entityFileName}-shell/${generator.entityFileName}-grid/${
+                                generator.entityFileName
+                            }-grid.component.ts`
+                    },
+                    {
+                        file: 'entities/entity-shell/entity-grid/entity-grid.component.scss',
+                        renameTo: generator =>
+                            `entities/${generator.entityFolderName}/${generator.entityFileName}-shell/${generator.entityFileName}-grid/${
+                                generator.entityFileName
+                            }-grid.component.scss`
+                    },
+                    {
+                        file: 'entities/entity-shell/entity-grid/entity-grid.component.html',
+                        method: 'processHtml',
+                        template: true,
+                        renameTo: generator =>
+                            `entities/${generator.entityFolderName}/${generator.entityFileName}-shell/${generator.entityFileName}-grid/${
+                                generator.entityFileName
+                            }-grid.component.html`
+                    },
+                    {
+                        file: 'entities/entity-shell/entity-grid/entity-card/entity-card.component.ts',
+                        renameTo: generator =>
+                            `entities/${generator.entityFolderName}/${generator.entityFileName}-shell/${generator.entityFileName}-grid/${
+                                generator.entityFileName
+                            }-card/${generator.entityFileName}-card.component.ts`
+                    },
+                    {
+                        file: 'entities/entity-shell/entity-grid/entity-card/entity-card.component.scss',
+                        renameTo: generator =>
+                            `entities/${generator.entityFolderName}/${generator.entityFileName}-shell/${generator.entityFileName}-grid/${
+                                generator.entityFileName
+                            }-card/${generator.entityFileName}-card.component.scss`
+                    },
+                    {
+                        file: 'entities/entity-shell/entity-grid/entity-card/entity-card.component.html',
+                        method: 'processHtml',
+                        template: true,
+                        renameTo: generator =>
+                            `entities/${generator.entityFolderName}/${generator.entityFileName}-shell/${generator.entityFileName}-grid/${
+                                generator.entityFileName
+                            }-card/${generator.entityFileName}-card.component.html`
+                    },
+                    {
+                        file: 'entities/entity-shell/entity-list/entity-list.component.ts',
+                        renameTo: generator =>
+                            `entities/${generator.entityFolderName}/${generator.entityFileName}-shell/${generator.entityFileName}-list/${
+                                generator.entityFileName
+                            }-list.component.ts`
+                    },
+                    {
+                        file: 'entities/entity-shell/entity-list/entity-list.component.scss',
+                        renameTo: generator =>
+                            `entities/${generator.entityFolderName}/${generator.entityFileName}-shell/${generator.entityFileName}-list/${
+                                generator.entityFileName
+                            }-list.component.scss`
+                    },
+                    {
+                        file: 'entities/entity-shell/entity-list/entity-list.component.html',
+                        method: 'processHtml',
+                        template: true,
+                        renameTo: generator =>
+                            `entities/${generator.entityFolderName}/${generator.entityFileName}-shell/${generator.entityFileName}-list/${
+                                generator.entityFileName
+                            }-list.component.html`
                     }
                 ]
             }
